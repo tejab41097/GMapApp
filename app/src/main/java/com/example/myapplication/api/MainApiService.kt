6baa3bdd00f4f9c62996f1e99395f0bfa982c2f3
Tejas Bhagwat <tejab41097@gmail.com>
2022-11-14 19:04:23 +0530

    Current Location Added.

--- app/src/main/java/com/example/myapplication/api/MainApiService.kt
+++
@@ -1,4 +0,0 @@
-package com.example.myapplication.api
-
-interface MainApiService {
-}
--- app/src/main/java/com/example/myapplication/fragment/MapFragment.kt
+++ app/src/main/java/com/example/myapplication/fragment/MapFragment.kt
@@ -1,9 +1,18 @@
 package com.example.myapplication.fragment
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -23,8 +32,11 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
 
     private var viewBinding: FragmentMapBinding? = null
     private val binding: FragmentMapBinding get() = viewBinding!!
-    private lateinit var map: GoogleMap
+    private var map: GoogleMap? = null
     private val mainViewModel: MainViewModel by activityViewModels()
+    private var locationByGps: Location? = null
+    private var locationByNetwork: Location? = null
+    private var currentLocation: Location? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -40,9 +52,9 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
         val mapFragment =
             childFragmentManager.findFragmentById(R.id.mapContainer) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
+        isLocationPermissionGranted()
         binding.fbAddMarker.setOnClickListener {
-            this.map.let {
+            this.map?.let {
                 mainViewModel.selectedMarker.postValue(
                     com.example.myapplication.database.model.Marker(
                         it.cameraPosition.target.latitude,
@@ -57,10 +69,104 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
         binding.fbList.setOnClickListener {
             findNavController().navigate(R.id.listFragment)
         }
+
+        binding.fbCurrentLocation.setOnClickListener {
+            isLocationPermissionGranted()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun loadCurrentLocation() {
+        val locationManager =
+            requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        val hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+        if (hasGps) {
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER, 5000, 0F, gpsLocationListener
+            )
+        }
+        if (hasNetwork) {
+            locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, 5000, 0F, networkLocationListener
+            )
+        }
+
+        val lastKnownLocationByGps =
+            locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        lastKnownLocationByGps?.let {
+            locationByGps = lastKnownLocationByGps
+        }
+        val lastKnownLocationByNetwork =
+            locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        lastKnownLocationByNetwork?.let {
+            locationByNetwork = lastKnownLocationByNetwork
+        }
+        if (locationByGps != null && locationByNetwork != null) {
+            currentLocation = if (locationByGps!!.accuracy > locationByNetwork!!.accuracy)
+                locationByGps
+            else
+                locationByNetwork
+            setLocation(currentLocation!!.latitude, currentLocation!!.longitude)
+        }
+    }
+
+    private fun setLocation(latitude: Double, longitude: Double) {
+        map?.let {
+            it.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    LatLng(latitude, longitude), it.cameraPosition.zoom
+                )
+            )
+        }
+    }
+
+    private val gpsLocationListener: LocationListener =
+        LocationListener { location -> locationByGps = location }
+
+    private val networkLocationListener: LocationListener =
+        LocationListener { location -> locationByNetwork = location }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        var permissionsGiven = true
+        permissions.entries.forEach {
+            if (!it.value)
+                permissionsGiven = false
+        }
+        if (permissionsGiven)
+            loadCurrentLocation()
+        else
+            Toast.makeText(requireContext(), "Permissions not granted", Toast.LENGTH_LONG)
+                .show()
+    }
+
+    private fun isLocationPermissionGranted() {
+        return if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissionLauncher.launch(
+                arrayOf(
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            )
+        } else
+            loadCurrentLocation()
     }
 
     override fun onMapReady(map: GoogleMap) {
         this.map = map
+        currentLocation?.let {
+            setLocation(it.latitude, it.longitude)
+        }
         mainViewModel.getAllMarkers().observe(viewLifecycleOwner) {
             map.clear()
             it.forEach { marker ->
@@ -71,15 +177,8 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListe
                 )
             }
             mainViewModel.showMarker.observe(viewLifecycleOwner) { marker ->
-                marker?.let {
-                    map.moveCamera(
-                        CameraUpdateFactory.newLatLngZoom(
-                            LatLng(
-                                marker.latitude,
-                                marker.longitude
-                            ), map.cameraPosition.zoom
-                        )
-                    )
+                marker?.let { safeMarker ->
+                    setLocation(safeMarker.latitude, safeMarker.longitude)
                     mainViewModel.showMarker.postValue(null)
                 }
 
